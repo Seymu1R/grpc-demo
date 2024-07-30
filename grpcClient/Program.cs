bb98a660be41d1b18b55f0d7e1bb296d25443a72
Seymur12 <seymur@cubics.technology>
2024-07-30 16:03:47 +0400

    practice grpc

--- grpcClient/Program.cs
+++ grpcClient/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Net.Client;
+using grpcMessageClient;
+
+namespace grpcClient
+{
+    class Program {
+        static async Task Main (string[] args){
+
+            var channel = GrpcChannel.ForAddress("http://localhost:5031");
+
+            var messageClient = new Message.MessageClient(channel);       
+
+            MessageReply reply = await messageClient.SendMessageAsync(new MessageRequest{
+                Message = "Hello",
+                Name = "Seymur"
+            });
+            Console.WriteLine(reply.Message);
+        }
+    }
+}
